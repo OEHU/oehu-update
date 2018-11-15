49f01216029a3e465ee2ca527db93eec8af383b3
bartwr <mail@bartroorda.nl>
2018-11-15 23:42:21 +0100

    Added update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+# Update oehu-setup-api
+cd ~/oehu-setup-api
+git pull
+npm install
+
+# Update oehu-website
+cd ~/oehu-website
+git pull
+npm install
+npm run build
+
+# Restart pm2 processes
+pm2 restart all
